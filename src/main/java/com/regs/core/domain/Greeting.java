891725d631a7288f446f13fa12d49e8821c6bfa4
home <adsfa@asdfa.com>
2014-03-02 20:26:37 +0300

    Edit constructor to fix initalization

--- src/main/java/com/regs/core/domain/Greeting.java
+++ src/main/java/com/regs/core/domain/Greeting.java
@@ -6,7 +6,7 @@ public class Greeting {
     private final String content;
 
     public Greeting(long id, String content) {
-        this.id = id +3;
+        this.id = id;
         this.content = content;
 
     }
